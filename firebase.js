32dce7c017b49f6c9abc2ff64704354a1e254340
wasupon <wasupon.mail@gmail.com>
2024-02-11 19:09:23 +0900

    Add: change env format

--- firebase.js
+++ firebase.js
@@ -9,12 +9,12 @@ import { getFirestore, collection } from "firebase/firestore";
 // use import.meta.env NOT process.env WHEN using Vite
 
 const firebaseConfig = {
-  apiKey: import.meta.env.APIKEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
@@ -22,5 +22,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const notesCollection = collection(db, "notes");
 
-console.log("appId:", import.meta.env.APP_ID); // returns undefined
+//console.log("appId:", import.meta.env.APP_ID); // returns undefined
 //To prevent accidentally leaking env variables to the client, only variables prefixed with VITE_ are exposed to your Vite-processed code.
+//https://vitejs.dev/guide/env-and-mode
